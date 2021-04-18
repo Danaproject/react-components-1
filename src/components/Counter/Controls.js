4ba39c0cdfd2a3c78a69cea8a537cb4477e41532
Danaproject <decoratio@gmail.com>
2021-04-18 14:26:38 +0200

    components

--- src/components/Counter/Controls.js
+++ src/components/Counter/Controls.js
@@ -1,10 +1,10 @@
 const Controls = ({ onIncrement, onDecrement }) => (
   <div className="Counter__controls">
-    <button type="button" onClick={onIncrement}>
-      + 1
-    </button>
     <button type="button" onClick={onDecrement}>
       - 1
+    </button>
+    <button type="button" onClick={onIncrement}>
+      + 1
     </button>
   </div>
 );
