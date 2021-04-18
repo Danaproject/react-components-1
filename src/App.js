a38b026a3c17e26c0c80c3740e9e91a8f4a034e8
Danaproject <decoratio@gmail.com>
2021-04-18 12:34:23 +0200

    react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Container from './components/Container/Container'
+import './styles.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1>Hey there!</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
