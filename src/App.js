f81900efa080397d229740a094dded0705cf3ea1
Danaproject <decoratio@gmail.com>
2021-04-18 14:22:43 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import Container from './components/Container/Container'
-import './styles.module.css';
+import Container from './components/Container/Container';
+import Counter from './components/Counter';
+import Dropdown from './components/Dropdown';
+import ColorPicker from './components/ColorPicker';
+import './styles.css';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <h1>Hey there!</h1>
-      </Container>
-    </div>
+    
+    <Container>
+      <h1>Hey there!</h1>
+      <div className="components">
+        <Counter initialValue={10} />
+        <Dropdown />
+        <ColorPicker options={colorPickerOptions} />
+      </div>
+    </Container>
+    
   );
 }
 
 export default App;
+
